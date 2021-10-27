8c143e752c73f4b5a55aac4363ddb9baf48b2c41
Zhike Chen <zk.chen007@gmail.com>
2021-10-27 10:58:00 +0800

    add detect_every_n_frame

--- supermarket_detection/config.py
+++ supermarket_detection/config.py
@@ -7,7 +7,8 @@ class ObjectDetection:
                  pipeline_config_path=None,
                  checkpoint_no=0,
                  label_map_path=None,
-                 min_score_thresh=0.3):
+                 min_score_thresh=0.3,
+                 detect_every_n_frame=80):
         self.load_model_from_checkpoint = load_model_from_checkpoint
         self.model_dir = model_dir
         self.pipeline_config_path = pipeline_config_path
@@ -15,6 +16,7 @@ class ObjectDetection:
         self.checkpoint_no = checkpoint_no
         self.label_map_path = label_map_path
         self.min_score_thresh = min_score_thresh
+        self.detect_every_n_frame = detect_every_n_frame
 
 
 class Config:
